123772e06eaddfd32c1075cfc11482271573674b
Isaiah Elston <ielston@uwyo.edu>
2024-04-22 13:48:25 -0600

    Testing my code on GitHub...

--- code.js
+++ code.js
@@ -1,3 +1,99 @@
-are_isomorphic(graph1, graph2) {
-    return undefined;
+/*
+Disclaimer: After spending a couple days working with this exercise, I did eventually reference Caden McFate's solution
+to help me figure it out. I did not copy any verbatim code, but I did follow their solution to help me finish this exercise.
+*/
+
+function swap(arr, left, right) {
+    let temp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temp;
+}
+
+function are_isomorphic(graph1, graph2) {
+
+    let graphLengthMatch = (graph1[0].length === graph2[0].length && graph1[1].length === graph2[1].length);
+    if (!graphLengthMatch) return false;
+
+    let fixedMatrix = adjacencyMatrix(graph1);
+    return tryPermutations(fixedMatrix, graph2, 0);
+}
+
+function tryPermutations(fixedMatrix, graph, initialNode) {
+
+    let permutedMatrix = adjacencyMatrix(graph);
+
+    if (areEquivalent(fixedMatrix, permutedMatrix)) {
+        return true;
+    }
+
+    let vertices = graph[0];
+
+    if (initialNode >= vertices.length - 1) {
+        return false;
+    }
+
+    for (let currentNode = initialNode; currentNode < vertices.length; currentNode++) {
+        if (currentNode !== initialNode) {
+            swap(vertices, initialNode, currentNode);
+            graph[0] = vertices.slice();
+        }
+
+        if (tryPermutations(fixedMatrix, graph, initialNode + 1)) {
+            return true;
+        }
+
+        if (currentNode !== initialNode) {
+            swap(vertices, initialNode, currentNode);
+
+            graph[0] = vertices.slice();
+        }
+    }
+    return false;
+}
+
+function adjacencyMatrix(graph) {
+
+    let vertices = graph[0];
+    let edges = graph[1];
+    let matrix = [];
+
+    for (let list = 0; list < vertices.length; list++) {
+
+        matrix[list] = [];
+
+        for (let element = 0; element < vertices.length; element++) {
+            matrix[list][element] = 0;
+        }
+    }
+
+    if (vertices.length === 0) {
+        return matrix;
+    }
+
+    for (let elementIndex = 0; elementIndex < edges.length; elementIndex++) {
+
+        let currentEdge = edges[elementIndex];
+        let left = currentEdge[0], right = currentEdge[1];
+        let leftIndex = vertices.indexOf(left);
+        let rightIndex = vertices.indexOf(right);
+
+        matrix[leftIndex][rightIndex] = 1;
+        matrix[rightIndex][leftIndex] = 1;
+    }
+    return matrix;
+}
+
+function areEquivalent(matrix1, matrix2) {
+    if (matrix1.length !== matrix2.length) {
+        return false;
+    }
+
+    for (let list = 0; list < matrix1.length; list++) {
+        for (let element = 0; element < matrix1[list].length; element++) {
+            if (matrix1[list][element] !== matrix2[list][element]) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
---
+++ code.test.js
@@ -0,0 +1,127 @@
+﻿const fs = require('fs');
+const jsc = require('jsverify');
+const assert = require('assert');
+
+eval(fs.readFileSync('code.js')+'');
+
+/*
+NOTE: I did look up some good examples rather than trying to come up with them myself, but I did write the tests myself.
+*/
+
+const pentagon = 
+[
+    [0,1,2,3,4],
+    [
+        [0,1],
+        [1,2],
+        [2,3],
+        [3,4],
+        [4,0]
+    ]
+];
+
+const star = 
+[
+    [0,1,2,3,4],
+    [
+        [0,2],
+        [2,4],
+        [4,1],
+        [1,3],
+        [3,0]
+    ]
+];
+
+const empty = [[],[]];
+const line = [[0,1,2],[[0,1],[1,2]]];
+
+const square = [
+    [0,1,2,3],
+    [
+        [0,1],
+        [1,2],
+        [2,3],
+        [3,0]
+    ]
+];
+
+const hourglass = [
+    [0,1,2,3],
+    [
+        [0,2],
+        [2,3],
+        [3,1],
+        [1,0]
+    ]
+];
+
+const pentaCross = [
+    [0,1,2,3,4],
+    [
+        [0,1],
+        [1,2],
+        [2,3],
+        [3,4],
+        [4,2],
+        [4,0]
+    ]
+];
+
+const complex = [
+    [0,1,2,3,4],
+    [
+        [0,1],
+        [0,3],
+        [1,2],
+        [2,0],
+        [2,3],
+        [0,4]
+    ]
+];
+
+const G = [
+    [0,1,2,3,4,5,6,7],
+    [
+        [0,1],
+        [0,3],
+        [0,5],
+        [1,2],
+        [1,4],
+        [2,3],
+        [2,7],
+        [3,6],
+        [4,5],
+        [4,7],
+        [5,6],
+        [6,7]
+    ]
+];
+
+const H = [
+    [0,1,2,3,4,5,6,7],
+    [
+        [0,1],
+        [1,2],
+        [2,3],
+        [3,0],
+        [4,5],
+        [5,6],
+        [6,7],
+        [7,4],
+        [6,2],
+        [1,5],
+        [7,3],
+        [4,0]
+    ]
+];
+
+
+
+assert(JSON.stringify(are_isomorphic(pentagon,star)) === JSON.stringify(true));
+assert(JSON.stringify(are_isomorphic(empty,line)) === JSON.stringify(false));
+assert(JSON.stringify(are_isomorphic(pentagon,line)) === JSON.stringify(false));
+assert(JSON.stringify(are_isomorphic(square,hourglass)) === JSON.stringify(true));
+assert(JSON.stringify(are_isomorphic(square, pentagon)) === JSON.stringify(false));
+assert(JSON.stringify(are_isomorphic(pentaCross, complex)) === JSON.stringify(false));
+assert(JSON.stringify(are_isomorphic(G,H)) === JSON.stringify(true));
+assert(JSON.stringify(are_isomorphic(G,G)) === JSON.stringify(true));
